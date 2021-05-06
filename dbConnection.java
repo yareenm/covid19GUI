12c3bcd2d980ba1744b419f5314dbd673a8c0249
Yaren Menemencioğlu <75498641+yareenm@users.noreply.github.com>
2021-05-06 22:09:59 +0300

    Update dbConnection.java

--- dbConnection.java
+++ dbConnection.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 
 public class dbConnection {
     private Connection connect;
-    private String url="jdbc:postgresql://localhost:5432/344_HMW2", user="postgres", password="Yaren1401";
+    private String url="jdbc:postgresql://localhost:5432/344_HMW2", user="postgres", password="YOUR_PASSWORD";
     
     public Connection dbConnect(){
         try{
