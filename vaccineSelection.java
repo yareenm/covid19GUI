eef0bc89edf922c9c25a7c2e89ecb6b068aa4f22
Yaren Menemencioğlu <75498641+yareenm@users.noreply.github.com>
2021-05-06 22:11:34 +0300

    Update vaccineSelection.java

--- vaccineSelection.java
+++ vaccineSelection.java
@@ -16,7 +16,7 @@ import javax.swing.table.DefaultTableModel;
 
 
 public class vaccineSelection extends javax.swing.JFrame {
-    private String url="jdbc:postgresql://localhost:5432/344_HMW2", user="postgres", password="Yaren1401";
+    private String url="jdbc:postgresql://localhost:5432/344_HMW2", user="postgres", password="YOUR_PASSWORD";
     private DefaultTableModel tblModel;
     private String username;
     
